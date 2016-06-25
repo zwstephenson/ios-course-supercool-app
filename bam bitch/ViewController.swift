dfa9e55053f7b197b85c2d4b087754d406f2c6ad
Zachry Stephenson <Zach@Zachrys-MBP.home>
2016-06-24 21:15:11 -0500

    why not update it all

--- bam bitch/ViewController.swift
+++ bam bitch/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var bg: UIImageView!
+    @IBOutlet weak var booTitle: UIImageView!
+    @IBOutlet weak var boomButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +21,11 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    @IBAction func makegoboom(sender: AnyObject) {bg.hidden = false
+        booTitle.hidden = false
+        boomButton.hidden = true
+        
     }
 
 
